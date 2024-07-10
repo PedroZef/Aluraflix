f76437df37c84bb6652cd5bf5184f79f695c9069
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2024-07-10 20:17:38 -0300

    Ajustes no styleGlobal.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import AppRoutes from "./routes"
+import "./styleGlobal.css"
+
 
 function App() {
     return (
         <>
+        <div className="App">
             <AppRoutes />
+        </div>
+        
         </>
     )
 }
